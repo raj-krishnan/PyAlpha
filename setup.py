e2ef5c35fd12962d3a637a43014f047a508f9a1d
Raj Krishnan <rajkrishnan1996@gmail.com>
2016-10-20 22:08:51 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+requires = ["numpy", 
+            "pandas", 
+            "peewee", 
+            "ystockquote"]
+
+setup(name='PyAlpha',
+      version='0.1',
+      description='Alpha Testing Utility',
+      author='Raj Krishnan',
+      author_email='rajkrishnan1996@gmail.com',
+      license='BSD',
+      install_requires=requires,
+      packages=['pyalpha', 
+                'pyalpha.data_structures', 
+                'pyalpha.portfolio'],
+     )
